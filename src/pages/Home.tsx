ff5120481100445843af815b0e4c88ccd7c667c7
Avan Futan <avanfutan@gmail.com>
2025-08-07 20:54:22 +0700

    feat: upload my cv

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,26 +3,23 @@ const HomePage = () => {
     <section id="home" className="flex flex-col py-20 text-center">
       <h2 className="text-4xl font-bold">Hi, I'm An Bui 👋</h2>
       <p className="mt-4 text-lg">
-        Experienced Full-Stack Developer with approximately 10 years of hands-on
-        experience in designing, building, and maintaining scalable applications
-        across both frontend and backend. Proficient in{" "}
+        Experienced Full-Stack Developer with approximately 10 years of hands-on experience in
+        designing, building, and maintaining scalable applications across both frontend and backend.
+        Proficient in{' '}
         <b>
-          React, NestJS, Node.js, Java, Spring Boot, JavaScript, TypeScript, and
-          Agile methodologies.
-        </b>{" "}
-        Passionate about solving real- world problems, embracing new
-        technologies, and working collaboratively with cross-functional teams to
-        deliver high-impact solutions.
+          React, NestJS, Node.js, Java, Spring Boot, JavaScript, TypeScript, and Agile
+          methodologies.
+        </b>{' '}
+        Passionate about solving real- world problems, embracing new technologies, and working
+        collaboratively with cross-functional teams to deliver high-impact solutions.
       </p>
       <div className="mt-6 space-x-4">
-        <a
-          href="#projects"
-          className="bg-blue-600 text-white px-4 py-2 rounded"
-        >
+        <a href="#projects" className="bg-blue-600 text-white px-4 py-2 rounded">
           View Projects
         </a>
         <a
-          href="/resume.pdf"
+          href="/An-Bui-Resume.pdf"
+          download
           className="border border-blue-600 text-blue-600 px-4 py-2 rounded"
         >
           Download CV
